04e3060b427c60bc8ece2bbb5099ef451d440137
Gareth Davidson <gaz@bitplane.net>
2026-09-08 17:13:51 +0100

    Recognize modern GCCSDK build hosts

--- src/mountin/builder/compiler/riscos/7800/build-toolbox.sh
+++ src/mountin/builder/compiler/riscos/7800/build-toolbox.sh
@@ -11,11 +11,18 @@ export GCCSDK_ROOT=$PWD
 . ./setup-gccsdk-params
 mkdir -p buildstepsdir
 
-make -j"$MOUNTIN_BUILD_JOBS" cross-gcc-built \
-    GCCSDK_INTERNAL_GETENV= \
-    GCC_LANGUAGES=c \
-    GCC_USE_SCM=no \
-    GCC_USE_PPL_CLOOG=no \
-    GCC_USE_LTO=no \
-    CROSS_ENABLE_SHARED=no \
+make_args=(
+    -j"$MOUNTIN_BUILD_JOBS"
+    GCCSDK_INTERNAL_GETENV=
+    GCC_LANGUAGES=c
+    GCC_USE_SCM=no
+    GCC_USE_PPL_CLOOG=no
+    GCC_USE_LTO=no
+    CROSS_ENABLE_SHARED=no
     GCC_CONFIG_ARGS='--enable-threads=posix --enable-sjlj-exceptions=no --enable-c99 --enable-cmath --disable-c-mbchar --disable-wchar_t --disable-libstdcxx-pch --disable-tls --enable-__cxa_atexit --enable-maintainer-mode --disable-werror --enable-interwork --disable-nls --disable-libquadmath --enable-checking=release --disable-multilib'
+)
+
+make "${make_args[@]}" src-automake-for-binutils-copied
+install -m 755 /usr/share/misc/config.guess /usr/share/misc/config.sub \
+    srcdir/automake-for-binutils/lib/
+make "${make_args[@]}" cross-gcc-built
